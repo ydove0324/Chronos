48643ac5798e70119f651858a53d629ac3a19a6f
y_dove <1352035939@qq.com>
2023-03-14 22:53:19 +0800

    update the auth

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL IN CLIENT ID";
+const CLIENT_ID = "1049307798941-3nuj3nndfu8520n1151qdiahodkpdgpe.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
