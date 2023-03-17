cd9db7c0298b2a58015bb53e8839a2fa10f67eca
y_dove <1352035939@qq.com>
2023-03-17 22:05:02 +0800

    PlanSchema and NewPostInput

---
+++ client/src/components/modules/Newplan.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { post } from "../../utilities";
+/**
+ * New Post is a parent component for all input components
+ *
+ * Proptypes
+ * @param {string} defaultText is the placeholder text
+ * @param {string} storyId optional prop, used for comments
+ * @param {({storyId, value}) => void} onSubmit: (function) triggered when this post is submitted, takes {storyId, value} as parameters
+ */
+const NewPostInput = (props) => {
+  const [value, setValue] = useState("");
+
+  // called whenever the user types in the new post input box
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  // called when the user hits "Submit" for a new post
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit && props.onSubmit(value);
+    setValue("");
+  };
+
+  return (
+    <div className="u-flex">
+      <input
+        type="text"
+        placeholder={props.defaultText}
+        value={value}
+        onChange={handleChange}
+        className="NewPostInput-input"
+      />
+      <button
+        type="submit"
+        className="NewPostInput-button u-pointer"
+        value="Submit"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+const Newplan = (props) => {};
--- server/models/plan.js
+++ server/models/plan.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
-const PlanSchema = new mongoose.Schema({});
+const PlanSchema = new mongoose.Schema({
+  creator_name: String,
+  creator_id: String,
+  start_time: Date,
+  end_time: Date,
+  plan_content: String,
+});
 module.exports = mongoose.model("plan", PlanSchema);
